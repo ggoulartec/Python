3a6e74745d3cffa2cdf7437aaa6663715e302466
Goularte G.C <92948398+ggoulartec@users.noreply.github.com>
2022-09-28 12:22:15 -0300

    Update main.py
    foi adicionado menu de contexto, classe usuário com os atributos.
    def init self desses atributos
    metodo menu para mostrar as opções pro usuário
    
    criado condicional para entrada de metodos de acordo com o que o usuario deseja
    
    um reload para a mensagem reaparecer em todos os metodos
    
    defini tambem os metodos de cadastrar e listar

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+import string, os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+lista_usuario = []
+
+class Usuario:
+    id: int
+    nome: string
+    idade: int
+    rg: string
+
+    def __init__(self, id, nome, idade, rg):
+        self.id = id
+        self.nome = nome
+        self.idade = idade
+        self.rg = rg
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def menu():
+    print("### Menu Sistema ###")
+    print("# 1. Cadastrar #")
+    print("# 2. Remover #")
+    print("# 3. Listar #")
+    print("# 4. Atualizar #")
+    print("# 5. Fechar #")
+    print("### ------&------ ###")
+    options = input("Digite a operação: ")
 
+    if options == "1":
+        cadastrar()
+    elif options == "2":
+        remover()
+    elif options == "3":
+        listar()
+    elif options == "4":
+        atualizar()
+    elif options == "5":
+        fechar()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def reload(mensagem: string, function):
+    options = input(mensagem + " [1-Sim/2-Não]: ")
+    if options == "1":
+        function()
+    elif options == "2":
+        menu()
+def cadastrar():
+    id = input("ID: ")
+    nome = input("NOME: ")
+    idade = input("IDADE: ")
+    rg = input("RG: ")
+    usuario = Usuario(id, nome, idade, rg)
+    lista_usuario.append(usuario)
+    print("Aluno cadastrado com sucesso")
+    reload("Gostaria de cadastrar uma nova pessoa?", cadastrar)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def remover():
+    id = input("Digite o ID: ")
+    for index, list in lista_usuario:
+        if list.id == id:
+            lista_usuario.remove(0)
+
+    print("Aluno Removido com sucesso")
+    reload("Gostaria de remover outro aluno?", remover)
+def listar():
+    for list in lista_usuario:
+        print(list)
+    reload("Gostaria listar novamente?", listar)
+def atualizar():
+    reload("Gostaria de atualizar mais pessoas?", atualizar)
+def fechar():
+    os.system('pause')
+def main():
+    menu()
+
+main()
